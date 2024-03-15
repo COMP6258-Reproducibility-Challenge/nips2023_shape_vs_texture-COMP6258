23f1c2225661b6bb1a590f7a0bc8979904e50ecf
Lianghong Ye <yelianghong@gmail.com>
2024-03-15 14:30:58 +0000

    simplify structure

--- model.py
+++ model.py
@@ -51,8 +51,9 @@ import torchvision.transforms as transforms
 from torchvision.models.vgg import VGG19_Weights
 import torch.nn as nn
 
-IMAGENET_MEAN = [0.485, 0.456, 0.406]
-IMAGENET_STD = [1,1,1]
+TARGET_LAYERS = [1,6] # 1,6,11,20,29
+IMAGE_SIZE = [100,100]
+EPHOCS = 1000
 
 def gram_matrix(input):
     b, c, h, w = input.size() # b=batch size, c=number of channels, (h, w)=dimensions of a feature map
@@ -63,28 +64,6 @@ def gram_matrix(input):
 # Contribution of layer l to the total loss
 def El (G, G_hat):
     return 1e9*torch.mean((G - G_hat)**2)
-# Total loss
-def L(layers, weights, Gs):
-    loss = 0
-    for i in range(len(layers)):
-        loss += weights[i] * El(Gs[i], gram_matrix(layers[i]))
-    return loss
-# derivative of El w.r.t. G_hat
-# def dEl(G, G_hat):
-#     b, c, h, w = G.size()
-#     return (G - G_hat) / (b * c * h * w)
-
-class Normalization(nn.Module):
-    def __init__(self, mean, std):
-        super(Normalization, self).__init__()
-        # reshape the mean as : [C x 1 x 1] so that it is compatible with image tensors of shape [B x C x H x W]
-        self.mean = torch.tensor(mean).view(-1, 1, 1)
-        self.std = torch.tensor(std).view(-1, 1, 1)
-
-    def forward(self, img):
-        # normalize img
-        return (img - self.mean) / self.std
-
 
 class TextureSynthesizer(nn.Module):
     def __init__(self, target_gram):
@@ -92,71 +71,56 @@ class TextureSynthesizer(nn.Module):
         self.target = target_gram
     def forward(self, input):
         G = gram_matrix(input)
-        #print(G.size())
         self.loss =  El(self.target, G)
         return input
-    
-def analyze(model, texture_img, target_layers=[1,6]): # 1,6,11,20,29
-    new_model = nn.Sequential(Normalization(IMAGENET_MEAN, IMAGENET_STD))
-    #new_model = nn.Sequential()
-    synthesizers = []
-    for i in range(len(model)):
-        if i > target_layers[-1]:
-            break
-        new_model.add_module(str(i), model[i])
-        if i in target_layers:
-            target_feature = new_model(texture_img).detach()
-            #print(target_feature.size())
-            G = gram_matrix(target_feature).detach()
-            #print(G.size())
-            synthesizer = TextureSynthesizer(G)
-            synthesizers.append(synthesizer)
-            new_model.add_module(str(i) + "_synthesizer", synthesizer)
-
-    return new_model, synthesizers
         
     
 
-class Vgg19Avg(nn.Module):
+class Model(nn.Module):
     def __init__(self):
-        super(Vgg19Avg, self).__init__()
+        super(Model, self).__init__()
         self.model = models.vgg19(weights=VGG19_Weights.DEFAULT).features
         # Replace maxpooling layers with average pooling
-        #self.synthesizer = TextureSynthesizer()
         for i in range(len(self.model)):
             if isinstance(self.model[i], nn.MaxPool2d):
-                self.model[i] = nn.AvgPool2d(kernel_size=2, stride=2, padding=0, ceil_mode=False)
-
+                self.model[i] = nn.AvgPool2d(kernel_size=self.model[i].kernel_size, stride=self.model[i].stride, padding=self.model[i].padding, ceil_mode=False)
 
     def analyze(self, texture_img):
-        synthesizer_model, synthesizers = analyze(self.model,texture_img)
+        synthesizer_model = nn.Sequential()
+        synthesizers = []
+        for i in range(max(TARGET_LAYERS) + 1):
+            synthesizer_model.add_module(str(i), self.model[i])
+            if i in TARGET_LAYERS:
+                target_feature = synthesizer_model(texture_img).detach()
+                G = gram_matrix(target_feature).detach()
+                synthesizer = TextureSynthesizer(G)
+                synthesizers.append(synthesizer)
+                synthesizer_model.add_module(str(i) + "_synthesizer", synthesizer)
+        print(synthesizer_model)
         return synthesizer_model, synthesizers
         # Weight rescaling is not needed, as per this report https://github.com/rpetit/texture-synthesis/blob/master/report.pdf
 
 
     
 def run_texture_synthesis(texture_img):
-    Vgg19Avg()
-    synthesizer_model, synthesizers =  Vgg19Avg().analyze(texture_img)
-    synthesized_img = torch.rand(1,3,100,100)
-    #rescale = transforms.Lambda(lambda x: (x - x.min()) / (x.max() - x.min()))
-    #synthesized_img = rescale(synthesized_img)
+    Model()
+    synthesizer_model, synthesizers =  Model().analyze(texture_img)
+    synthesized_img = torch.rand(1,3,IMAGE_SIZE[0],IMAGE_SIZE[1])
     optimizer = optim.LBFGS([synthesized_img.requires_grad_(True)])
 
     run = [0]
-    while run[0] < 2000:
+    while run[0] < EPHOCS:
         def closure():
             optimizer.zero_grad()
             synthesizer_model(synthesized_img)
             texture_loss = 0
             for s in synthesizers:
                 texture_loss += s.loss
-            loss = texture_loss
-            loss.backward()
+            texture_loss.backward()
             run[0] += 1
-            if run[0] % 10 == 0:
+            if run[0] % (EPHOCS / 10) == 0:
                 print("run {}:".format(run))
-                print('Loss : {:4f}'.format(loss.item()))
+                print('Loss : {:4f}'.format(texture_loss.item()))
                 print()
             return texture_loss
         optimizer.step(closure)
@@ -166,7 +130,7 @@ def run_texture_synthesis(texture_img):
 
 texture = 'img.jpg'
 texture_image_orig = Image.open(texture)
-texture_image_orig = texture_image_orig.resize((100, 100))
+texture_image_orig = texture_image_orig.resize((IMAGE_SIZE[0], IMAGE_SIZE[1]))
 to_tensor = transforms.ToTensor()
 
 texture_image = to_tensor(texture_image_orig)
